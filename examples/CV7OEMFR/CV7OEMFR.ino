ebb854a8d1989400433c0bde4993239b94a7ebef
maxime.lebegue <maxime.lebegue@laposte.net>
2022-05-05 14:06:27 +0200

    modif code

--- examples/CV7OEMFR/CV7OEMFR.ino
+++ examples/CV7OEMFR/CV7OEMFR.ino
@@ -1,80 +1,71 @@
 #include "CV7OEMFR.h"
 #include <EEPROM.h>
 #define RXD 16
-#define TXD 17
 #define EEPROM_SIZE 10
 
-int i = 0;        //Déclaration des variables
-bool Temp = false, Vit = false;
+int i = 0;        //Variable declaration
+bool one = false, two = false;
 String static strIdModule;
 
 void setup()
 { 
-  Serial.begin(115200);
-  Serial2.begin(4800,SERIAL_8N1,RXD,TXD);
-  EEPROM.begin(EEPROM_SIZE);
+ Serial.begin(115200);
+ Serial2.begin(4800,SERIAL_8N1,RXD);
+ EEPROM.begin(EEPROM_SIZE);
 
  String yesno;
  strIdModule = EEPROM.read(0);
  int result;
  
- Serial.println("L'ancien module est : " + strIdModule);
- Serial.println("Voulez-vous saisir un nouveau module ? (y/n)");
+ Serial.println("The old module is : " + strIdModule);
+ Serial.println("Do you want to enter a new module ? (y/n)");
  
  Serial.setTimeout(10000);
  yesno = Serial.readStringUntil('\n');
  
  if(yesno == "n" || yesno == 0)
  {
-    Serial.println("Vous êtes sur le module : " + strIdModule);
+  Serial.println("You are on the module : " + strIdModule);
  }
  else
  {
-  Serial.println("Saisir le nouveau module : ");
+  Serial.println("Enter the new module : ");
   strIdModule = Serial.readStringUntil('\n');
   
-  //result = strIdModule.toInt();  //converti un string en char
-  
-  EEPROM.write(0, strIdModule.toInt());  //deuxième valeur ne doit pas être un string 
+  EEPROM.write(0, strIdModule.toInt());  //second value must not be a string
   EEPROM.commit();
-  Serial.print("Le nouveau module est : "); Serial.println(strIdModule.toInt());
+  Serial.print("The new module is : "); Serial.println(strIdModule.toInt());
  }
  delay(5000);
         
 }
 void loop()
 {
-      String value=Serial2.readStringUntil('\n');
-      Serial.print("Trame : ");
-      Serial.println(value);
-      float temp;
-      float dirvent;
-      float vitesse;
-     
-      //float idCapteur = 1;
+ String value=Serial2.readStringUntil('\n');
+ //Serial.println(value);  //displays the entire frame
+ float temp, dirvent, speed;
 
-      CV7OEMFR* trame;  //Déclaration d'un pointeur dans la classe CV7OEMFR
-      trame = new CV7OEMFR(value);    //Une fois sur deux la trame va prendre soit la valeur du paramètre 1 soit du paramètre 2
-
-      if (trame->getTypeTrame() == 1)        //On appelle la fonction getTypeTrame() pour savoir si la trame est de format "$IIMWV" ou "$WIXDR"
-      {
-          dirvent = trame->getDirection();      // On sauvegarde les valeurs de getDirection() et getVitesse() dans les variables attendues
-          vitesse = trame->getVitesse();
-          Vit = true;       //dirVit passe à true quand la trame est de format "$IIMWV"
-      }
-      if (trame->getTypeTrame() == 2)       //On appelle la fonction getTypeTrame() pour savoir si la trame est de format "$IIMWV" ou "$WIXDR"
-      {
-          temp = trame->getTemperature();       //On sauvegarde les valeurs de getTemperature() dans la variables attendue
-          Temp = true;      //temp passe à true quand la trame est de format "$IWXDR"
-      }
-        if (Vit == true && Temp == true)      // Quand les deux sont true, sauvegarde les résultats
-      {
-          Serial.print("La direction est de : ");
-          Serial.println(dirvent);
-          Serial.print("La vitesse est de : ");
-          Serial.println(vitesse);
-          Serial.print("la température est de : ");
-          Serial.println(temp);
-          Vit = false; Temp = false;        // temp et dirVit passe à false et recommance dans la boucle
-      }
+ CV7OEMFR* fram;  //Declaring a pointer in the CV7OEMFR class
+ fram = new CV7OEMFR(value);    //Half the time the frame will take either the value of parameter 1 or parameter 2
+ if(fram->getFrameType() == 1)        //We call the getTypeFrame() function to find out if the frame is in "$IIMWV" or "$WIXDR" format
+ {
+  dirvent = fram->getDirection();      //We save the values ​​of getDirection() and getVitesse() in the expected variables
+  speed = fram->getSpeed();
+  one = true;       //dirVit goes to true when the frame is in "$IIMWV" format
+ }
+  if(fram->getFrameType() == 2)       //We call the getTypeFrame() function to find out if the frame is in "$IIMWV" or "$WIXDR" format
+  {
+   temp = fram->getTemperature();       //We save the values ​​of getTemperature() in the expected variable
+   two = true;      //temp goes to true when the frame is in "$IWXDR" format
+  }
+   if (one == true && two == true)      // When both are true, save the results
+   {
+    Serial.print("The direction is : ");
+    Serial.println(dirvent);
+    Serial.print("The speed is : ");
+    Serial.println(speed);
+    Serial.print("The temperature is : ");
+    Serial.println(temp);
+    one = false; two = false;        //temp and dirVit go to false and start again in the loop
+   }
 }
